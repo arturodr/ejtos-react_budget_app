97fac168bab3051336f53a0d8a1fdb61a3e2cc3d
Arturo Diaz Rosemberg <arturodr@gmail.com>
2024-03-01 01:47:37 -0500

    Decrease and Increase Buttons

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import { faCircleMinus} from '@fortawesome/free-solid-svg-icons';
 
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
@@ -25,11 +28,25 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button className='btn' onClick={event=> increaseAllocation(props.name)}><FontAwesomeIcon icon={faCirclePlus}  size='lg' style={{color: "#4FAC5C",}} /></button></td>
+        <td><button className='btn' onClick={event=> decreaseAllocation(props.name)}><FontAwesomeIcon icon={faCircleMinus} size='lg' style={{color: "#AF241A",}} /></button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
