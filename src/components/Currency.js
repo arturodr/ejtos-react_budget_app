334082174e0424163b2a4e27ea299d5266b8d3a9
Arturo Diaz Rosemberg <arturodr@gmail.com>
2024-03-01 01:26:41 -0500

    Currency prefix to the Change allocation textbox

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,15 +1,21 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Currency = () => {
-    const { currency } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
     const [newCurrency, setNewCurrency] = useState(currency);
 
     const handleCurrencyChange = (event) => {
-        setNewCurrency(event.target.value);
+        const selectedCurrency = event.target.value;
+        console.log(selectedCurrency)
+        setNewCurrency(selectedCurrency);
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: selectedCurrency
+        });
     }
     return (
         <div className='alert alert-warning'>
-            <select className="form-select mb-3 bg-success text-white" id="inputGroupSelect04" onChange={(event) => handleCurrencyChange(event.target.value)} defaultValue="$">
+            <select className="form-select mb-3 bg-success text-white" id="inputGroupSelect04" onChange={(event) => handleCurrencyChange(event)} defaultValue="£">
                 <option value="$" name="dollar">$ Dollar</option>
                 <option value="£" name="pound">£ Pound</option>
                 <option value="€" name="euro">€ Euro</option>
