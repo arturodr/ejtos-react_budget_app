54c1db94d9266df453f6a9bb86cd494d1f00d989
Arturo Diaz Rosemberg <arturodr@gmail.com>
2024-03-01 01:29:02 -0500

    Currency prefix to the Budget value textbox

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, expenses } = useContext(AppContext);
+    const { budget, expenses, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         const totalExpenses = expenses.reduce((total, item) => {
@@ -12,17 +12,17 @@ const Budget = () => {
         console.log("test: " + newValue + " < "+ totalExpenses);
 
         if (newValue > 20000) {
-            alert("The Budget can't be larger than 20000");
+            alert("The Budget can't be larger than " + {currency} + "20000");
             event.target.value = 20000;
         } else if (newValue < totalExpenses) {
-            alert("The Budget can't be smaller than "+totalExpenses);
+            alert("The Budget can't be smaller than " + {currency} + totalExpenses);
             event.target.value = totalExpenses;
         }
         setNewBudget(event.target.value);
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: {currency}{budget}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
