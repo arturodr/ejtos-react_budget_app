95894e08fac2a5ded01beddfc61224b36b730d81
Arturo Diaz Rosemberg <arturodr@gmail.com>
2024-03-01 00:31:41 -0500

    Editable budget value

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,9 +2,22 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost);
+        }, 0);
+        const newValue = parseInt(event.target.value);
+        console.log("test: " + newValue + " < "+ totalExpenses);
+
+        if (newValue > 20000) {
+            alert("The Budget can't be larger than 20000");
+            event.target.value = 20000;
+        } else if (newValue < totalExpenses) {
+            alert("The Budget can't be smaller than "+totalExpenses);
+            event.target.value = totalExpenses;
+        }
         setNewBudget(event.target.value);
     }
     return (
